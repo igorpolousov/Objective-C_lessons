6acc44f4cec18d98b32ec01224214ce37c43c224
Igor Polousov <igorpolousov@gmail.com>
2024-08-23 17:00:02 +0300

    ну что же, попробуем objective-c with Storyboard then test OC with in code IB if it exists

--- Objective-C_lessons/ViewController.m
+++ Objective-C_lessons/ViewController.m
@@ -8,6 +8,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *HelloWorldLabel;
+@property (weak, nonatomic) IBOutlet UILabel *headerFile;
 
 @end
 
@@ -16,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _HelloWorldLabel.text = @"Oh, it works almost like with Swift";
+    _headerFile.text = @"За кой нам нужен Header file пока не понятно 🥳";
+    
 }
 
 
--- Objective-C_lessons/main.m
+++ Objective-C_lessons/main.m
@@ -8,10 +8,12 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+// Initial function
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
+        NSLog(@"Hello Developers, welcome to Objective-c");
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
